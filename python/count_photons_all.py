d6c551f8e1eee3c7b6df3a614a16aa3eb746d678
OTPS3141 <otschmidt@ethz.ch>
2021-12-14 13:10:20 +0100

    count photons

--- python/count_photons_all.py
+++ python/count_photons_all.py
@@ -6,38 +6,64 @@ from read_bin import read, plot
 Read data from statistics
 '''
 
-df = read("Data\Photon statistics\statistics\pm_pos1_1\pm_pos1_1_1.csv")
-time, A, B = df.read_data()
+data_list = []
+
+for i in range(5):
+
+    data_list.append(read(f"Data\Photon statistics\statistics\pm_pos{i+1}_1\pm_pos{i+1}_1_1.csv"))
+
+
+time, A, B = data_list[0].read_data()
+
+'''
+There is no A channel for the other data sets pm_pos(2-4)_1_1.csv!
+'''
+
+# time_list = []
+# A_list = []
+# B_list =[]
+
+# for i in range(5):
+
+#     t, A, B = data_list[i].read_data()
+#     time_list.append(t)
+#     A_list.append(A)
+#     B_list.append(B)
+
+
 
 
 # plot(time, A, B)
+
+plt.plot(time, A)
+plt.show()
 
 # Number of observations
 N = time.size
 
 
 '''
-Split the 1000 ms in n equal bins 
+Split the 1000 ms in n equal bins
 '''
 
 def bins(n):
-    
+
     bins = []
-    
+
     a = 0
     b = (N // n) - 1
-    
+
     for i in range(0, n):
-        
-        
-        
+
+
+
         bins.append(A[a:b])
-        
+
         a = b + 1
         b = b + (N // n)
-        
+
         # print(a, b)
-    
+
     return bins
 
 # print(bins(100)[89].size)
@@ -60,31 +86,31 @@ Which values does the TTL signal take?
 #     return values
 
 # values = values_in_TTL(1000)
-    
+
 # print(B)
 
 
 # def find_photon_thresh():
-    
+
 #     counts = np.zeros(np.linspace(5.3, 6, 100).size)
 
 
 #     for n, b in enumerate(np.linspace(5.3, 6, 100)):
-        
+
 #         counter = 0
 #         # print(n, b)
-    
+
 #         for i in range(0, 976567):
-            
+
 #             if B[i] > b:
 #                 counter += 1
-            
-#         counts[n] = counter  
-    
+
+#         counts[n] = counter
+
 #     return counts
 
 # counts = find_photon_thresh()
-    
+
 # plt.plot(np.linspace(5.3, 6, 100), counts, 'o')
 
 # good_cut = np.where(counts < 2000)
@@ -111,43 +137,50 @@ def count_TTL(TTL):
 
     for i in range(N):
 
-        if TTL[i] == 0:
+        if TTL[i] > -0.005:
             counter += 1
 
     return counter
 
+
+# for i in range(5):
+
+#     print(f"The data set {i+1} contains {time_list[i].size} observations.")
+#     print(count_TTL(A_list[i]), "TTL rectangles were counted")
+
+print(f"The data set contains {time.size} observations.")
 print(count_TTL(A), "TTL rectangles were counted")
 
 
 ### Count the TTL rect in bins
 
 def count_bins(n):
-    
+
     counter_bin = []
-    
+
     for i in range(0, n):
-        
-        counter = 0 
-        
-    
-        
+
+        counter = 0
+
+
+
         for j in range(0, (N // n) - 1):
-            
+
             if bins(n)[i][j] == 0:
-                
+
                 counter += 1
-    
+
         counter_bin.append(counter)
-    
+
     photons = np.array(counter_bin)
-    
+
     mean = np.mean(photons)
-    
+
     sigma = np.std(photons)
-    
+
     print(*["Mean:", mean, " and standard deviation:", sigma, "for", n, "bins"])
-    
-    
+
+
     return photons
 
 
@@ -156,11 +189,17 @@ def count_bins(n):
 '''
 Compare different amount of bins
 '''
-for i in [5, 10, 15, 20, 30, 40, 50, 60, 70, 80, 90, 100]:
-    
-    photons = count_bins(i)
-    plt.figure()
-    plt.hist(photons, bins = i, density = True, label = "Data")
-    plt.title(f"Histogram for {i} bins with duration {np.round(1000 / i, 2)} ms")
 
-plt.show()
+def different_bins():
+
+
+    for i in [5, 10, 15, 20, 30, 40, 50, 60, 70, 80, 90, 100]:
+
+        photons = count_bins(i)
+        plt.figure()
+        plt.hist(photons, bins = i, density = True, label = "Data")
+        plt.title(f"Histogram for {i} bins with duration {np.round(1000 / i, 2)} ms")
+
+    plt.show()
+
+# different_bins()
