04ad1324edc184e855fa89ce77cd61889812a26b
OTPS3141 <otschmidt@ethz.ch>
2021-12-15 19:05:05 +0100

    photon count

---
+++ python/count_photons_diff.py
@@ -0,0 +1,179 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from read_bin import read, plot
+from scipy.optimize import curve_fit
+from scipy.special import factorial
+from scipy.stats import poisson
+import matplotlib.ticker as mticker
+
+
+'''
+Read data from statistics
+'''
+
+data_list = []
+
+for i in range(5):
+
+    data_list.append(read(f"Data\Photon statistics\statistics\pm_pos{i+1}_1\pm_pos{i+1}_1_1.csv"))
+
+time, B = data_list[0].read_data()
+
+N = time.size
+
+print(f"We have {N} observations")
+
+
+def count(PM):
+    '''
+    Counts the photons from PM signal
+    '''
+
+    N = PM.size
+
+    counter = np.zeros(N)
+
+
+    for i in range(N):
+
+        if PM[i] >= 20:
+
+            counter[i] = 1
+
+
+    photon = 0
+
+    for i in range(1, N):
+
+        if counter[i] == 1 and counter[i-1] == 0:
+
+            photon += 1
+
+    return photon
+
+photons = count(B)
+
+print(f"We count {photons} photons")
+
+
+def bins(n):
+
+    bins = []
+
+    a = 0
+    b = (N // n) - 1
+
+    for i in range(0, n):
+
+
+
+        bins.append(B[a:b])
+
+        # print(a, b)
+        a = b + 1
+        b = b + (N // n)
+
+        
+
+    return bins
+
+def count_bins(PM, n):
+
+    counter_bin = []
+
+    for i in range(n):
+
+        counter_bin.append(count(bins(n)[i]))
+
+    photons = np.array(counter_bin)
+
+    return photons
+
+
+
+'''
+Compare different amount of bins
+'''
+
+def different_bins():
+
+
+    for i in [5, 10, 15, 20, 30, 40, 50, 60, 70, 80, 90, 100, 150]:
+
+        photons = count_bins(bins(i), i)
+        print(f"Mean: {np.mean(photons)} and std. deviation: {np.std(photons)} for {i} bins")
+        plt.figure()
+        plt.hist(photons, bins = i, density = False, label = "Data")
+        plt.title(f"Histogram for {i} bins with duration {np.round(1000 / i, 2)} ms")
+
+    plt.show()
+
+# different_bins()
+
+# print(count(bins(30)[0]))
+
+'''
+Fit to poissonian distribution
+'''
+
+def fit(n):
+
+
+    gamma = count_bins(bins(n), n)
+
+    ### What is the suitable number of bins?
+
+    values = np.unique(gamma)
+    number_of_bins = np.unique(gamma).size
+    min = np.min(np.unique(gamma))
+    max = np.max(np.unique(gamma))
+    x_plot = np.arange(min, max + 1)
+    mean = np.round(np.mean(gamma), 3)
+    std = np.round(np.std(gamma), 3)
+
+
+    # print(number_of_bins)
+
+    entries, bin_edges, patches = plt.hist(gamma, bins=x_plot, density=True, label='Histogram of events', align='mid')
+
+    print(f"For {n} bins:")
+
+    print(f"Values {values} and entries {entries}")
+
+    bin_middles = 0.5 * (bin_edges[1:] + bin_edges[:-1])
+
+    print(f"Bin middles {bin_middles} and bin edges {bin_edges}")
+
+    def fit_function(k, lamb):
+        '''poisson function, parameter lamb is the fit parameter'''
+        return lamb**k / factorial(k) * np.exp(-lamb)
+
+    x_plot = np.arange(min, max + 1)
+
+
+    # fit with curve_fit
+    parameters, cov_matrix = curve_fit(fit_function, bin_middles, entries)
+    std_fit = np.sqrt(np.diag(parameters))
+
+    print(f"Mean ({mean}, {parameters[0]}) and std. deviation ({std}, {std_fit[0]})")
+    
+    plt.plot(
+        x_plot,
+        fit_function(x_plot, *parameters),
+        '-o',
+        label='Fitting to Poissonian'
+    )
+    plt.title(f"Histogram for {n} bins with $\mu = {mean}$ and $\sigma = {std}$")
+    # plt.text(min, np.max(entries), f"$\mu = {mean}$ and $\sigma = {std}$")
+    plt.gca().xaxis.set_major_locator(mticker.MultipleLocator(1))
+    plt.xlabel("Number of events (photons)")
+    plt.ylabel("Probability density")
+    plt.legend(loc="best")
+    
+
+# , 60, 70, 80, 90, 100, 125, 150, 175
+for i in [15, 20, 30, 40, 50 , 60, 70, 80, 90, 100, 125, 150, 175]:
+    plt.figure()
+    fit(i)
+
+plt.show()
--- python/read_bin.py
+++ python/read_bin.py
@@ -18,31 +18,31 @@ class read:
 
     def read_data(self):
 
-        self.df = pd.read_csv(self.data, delimiter=',', names = ['time', 'channel_A', 'channel_B'], low_memory=False) 
+        self.df = pd.read_csv(self.data, delimiter=',', names = ['time', 'channel_A', 'channel_B'], low_memory=False, usecols=["time", "channel_B"]) 
         self.df = self.df.drop([0,1,2], axis=0)
         
         self.time = np.array(self.df.time).astype(np.float)
-        self.A = np.array(self.df.channel_A).astype(np.float)
         self.B = np.array(self.df.channel_B).astype(np.float)
         
-        return self.time, self.A, self.B
+        return self.time, self.B
 
     
-
+# df_noclass = pd.read_csv("Data\Photon statistics\statistics\pm_pos1_1\pm_pos1_1_1.csv", delimiter=',', names = ['time', 'channel_A', 'channel_B'], low_memory=False, usecols=["time", "channel_B"]) 
 df = read("Data\Photon statistics\statistics\pm_pos1_1\pm_pos1_1_1.csv")
-time, A, B = df.read_data()
+time, B = df.read_data()
 
 
-def plot(time, A, B):
 
-    plt.figure()
-    plt.plot(time, A ,'-', label = "PM TTL")
-    plt.legend(loc="best")
-    plt.show()
+def plot(time, B):
+
+
 
     plt.figure()
     plt.plot(time, B,'-', label = "PM Signal", color = 'red')
     plt.legend(loc="best")
+    plt.xlabel("Time [s]")
+    plt.ylabel("Voltage [mV]")
     plt.show()
 
-# plot(time, A, B)
+
+plot(time, B)
